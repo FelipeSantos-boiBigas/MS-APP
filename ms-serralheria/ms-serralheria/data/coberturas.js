2f297964c3e1c89cbe28579c05012edab8259018
FelipeSantos-boiBigas <felipehsantos113@gmail.com>
2026-05-11 10:12:23 -0300

    Add files via upload

---
+++ ms-serralheria/ms-serralheria/data/coberturas.js
@@ -0,0 +1,27 @@
+const coberturas = [
+
+  {
+    item: "Cobertura",
+    category: "Coberturas",
+    price: "9800",
+    material: "Aço galvanizado",
+    dimensoes: "20m x 20m",
+    descricao: "Cobertura para proteção e construções",
+    imagem: require("../assets/cobertura/cb01.png"),
+    id: 1,
+  },
+
+{
+    item: "Cobertura",
+    category: "Coberturas",
+    price: "7500",
+    material: "Aço galvanizado",
+    dimensoes: "20m x 15m",
+    descricao: "Cobertura para proteção e construções",
+    imagem: require("../assets/cobertura/cb02.png"),
+    id: 2,
+  },
+
+];
+
+export default coberturas;
---
+++ ms-serralheria/ms-serralheria/data/grades.js
@@ -0,0 +1,16 @@
+const grades = [
+
+  {
+    item: "Grade de proteção",
+    category: "Grades de Proteção",
+    price: "800",
+    material: "Aço galvanizado",
+    dimensoes: "2m x 2m",
+    descricao: "Grade de proteçao para portas e janelas",
+    imagem: require("../assets/grade/gr1.png"),
+    id: 1,
+  },
+
+];
+
+export default grades;
