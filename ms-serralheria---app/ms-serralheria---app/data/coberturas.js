7cda31e7528eaf727dd8876d60131ae9cd797f9d
FelipeSantos-boiBigas <felipehsantos113@gmail.com>
2026-05-11 14:21:31 -0300

    Add files via upload

---
+++ ms-serralheria---app/ms-serralheria---app/data/coberturas.js
@@ -0,0 +1,27 @@
+const coberturas = [
+
+  {
+    item: "Cobertura",
+    category: "Coberturas",
+    price: "7800",
+    material: "Aço galvanizado",
+    dimensoes: "20m x 20m",
+    descricao: "Cobertura para proteção e construções",
+    imagem: require("../assets/cobertura/cb01.png"),
+    id: 22,
+  },
+
+{
+    item: "Cobertura",
+    category: "Coberturas",
+    price: "10000",
+    material: "Aço galvanizado",
+    dimensoes: "20m x 15m",
+    descricao: "Cobertura para proteção e construções",
+    imagem: require("../assets/cobertura/cb02.png"),
+    id: 23,
+  },
+
+];
+
+export default coberturas;
---
+++ ms-serralheria---app/ms-serralheria---app/data/portoesBasculantes.js
@@ -0,0 +1,114 @@
+const portoesBasculantes = [
+
+  {
+    item: "Basculante B01",
+    category: "Portões Basculantes",
+    price: "2500",
+    material: "Aço galvanizado",
+    dimensoes: "3m x 2.5m",
+    descricao: "Portão resistente para garagem",
+    imagem: require("../assets/basculantes/b01.png"),
+    id: 9,
+  },
+
+  {
+    item: "Deslizante B02",
+    category: "Portões Basculantes",
+    price: "3200",
+    material: "Alumínio",
+    dimensoes: "4m x 2.5m",
+    descricao: "Portão moderno e automático",
+    imagem: require("../assets/basculantes/b02.png"),
+    id: 10,
+  },
+
+{
+    item: "Basculante B03",
+    category: "Portões Basculantes",
+    price: "4500",
+    material: "Aço galvanizado",
+    dimensoes: "3m x 2m",
+    descricao: "Portão resistente para garagem",
+    imagem: require("../assets/basculantes/b03.png"),
+    id: 11,
+  },
+
+  {
+    item: "Basculante B04",
+    category: "Portões Basculantes",
+    price: "3500",
+    material: "Aço galvanizado",
+    dimensoes: "4m x 2m",
+    descricao: "Portão resistente para garagem",
+    imagem: require("../assets/basculantes/b04.png"),
+    id: 12,
+  },
+
+  {
+    item: "Basculante B05",
+    category: "Portões Basculantes",
+    price: "3000",
+    material: "Aço galvanizado",
+    dimensoes: "3,5m x 2.5m",
+    descricao: "Portão resistente para garagem",
+    imagem: require("../assets/basculantes/b05.png"),
+    id: 13,
+  },
+
+  {
+    item: "Basculante B06",
+    category: "Portões Basculantes",
+    price: "2300",
+    material: "Aço galvanizado",
+    dimensoes: "3,5m x 2.8m",
+    descricao: "Portão resistente para garagem",
+    imagem: require("../assets/basculantes/b06.png"),
+    id: 14,
+  },
+
+  {
+    item: "Basculante B07",
+    category: "Portões Basculantes",
+    price: "2900",
+    material: "Aço galvanizado",
+    dimensoes: "3m x 3m",
+    descricao: "Portão resistente para garagem",
+    imagem: require("../assets/basculantes/b07.png"),
+    id: 15,
+  },
+
+  {
+    item: "Basculante B08",
+    category: "Portões Basculantes",
+    price: "2200",
+    material: "Aço galvanizado",
+    dimensoes: "3m x 2.5m",
+    descricao: "Portão resistente para garagem",
+    imagem: require("../assets/basculantes/b08.png"),
+    id: 16,
+  },
+
+  {
+    item: "Basculante B09",
+    category: "Portões Basculantes",
+    price: "2550",
+    material: "Aço galvanizado",
+    dimensoes: "5m x 3m",
+    descricao: "Portão resistente para garagem",
+    imagem: require("../assets/basculantes/b09.png"),
+    id: 17,
+  },
+
+  {
+    item: "Basculante B010",
+    category: "Portões Basculantes",
+    price: "2800",
+    material: "Aço galvanizado",
+    dimensoes: "3m x 2.5m",
+    descricao: "Portão resistente para garagem",
+    imagem: require("../assets/basculantes/b10.png"),
+    id: 18,
+  },
+];
+
+export default portoesBasculantes;
